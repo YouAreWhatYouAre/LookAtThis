4b7a17067f0177a445cec3d9dc68c01e0b5575ea
Dmitriy Surovyev <dmitriysurovev@gmail.com>
2023-12-06 16:37:24 +0300

    design fix

--- you-are-what-you-are/src/App.js
+++ you-are-what-you-are/src/App.js
@@ -1,23 +1,8 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Кто двинется - **дор
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
       <div className='container'>
         <p className='container__main-caption'>Ты пидор!</p>
         <p className='container__additional-caption'>
