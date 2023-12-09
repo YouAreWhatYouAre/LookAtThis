46a75da7969172ed8f1d238537eda395a3116886
Victoria Markova <vii.nuncame@gmail.com>
2023-12-10 00:33:51 +0300

    fix: design

--- you-are-what-you-are/src/App.js
+++ you-are-what-you-are/src/App.js
@@ -1,8 +1,10 @@
+import Hand from "./hand.png";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
+      <img className='background-img' src={Hand} alt='hand' />
       <div className='container'>
         <p className='container__main-caption'>Ты пидор!</p>
         <p className='container__additional-caption'>
@@ -12,6 +14,24 @@ function App() {
           Попалось:
           <span className='container__quantity-number'> xxx</span>
         </p>
+      </div>
+      <div className='contacts'>
+        <a
+          className='contacts__telega'
+          href='https://t.me/nuncame'
+          target='_blank'
+          rel='noreferrer'
+        >
+          @vii
+        </a>
+        <a
+          className='contacts__telega'
+          href='https://t.me/DmitryAlabam'
+          target='_blank'
+          rel='noreferrer'
+        >
+          @alabam
+        </a>
       </div>
     </div>
   );
