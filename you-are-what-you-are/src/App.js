df99ca6098913efb7ed16ad803ab17936869d4cc
Victoria Markova <vii.nuncame@gmail.com>
2023-12-21 22:38:23 +0300

    feat: animation&api

--- you-are-what-you-are/src/App.js
+++ you-are-what-you-are/src/App.js
@@ -1,7 +1,20 @@
+import { useState, useEffect } from "react";
 import Hand from "./hand.png";
 import "./App.css";
+import "./vendor/normalize.css";
+import "./vendor/fonts/fonts.css";
+import countapi from "countapi-js";
 
 function App() {
+  const [visitors, setVisitors] = useState(0);
+
+  useEffect(() => {
+    countapi.visits().then((result) => {
+      console.log(result.value);
+      setVisitors(result.value);
+    });
+  }, []);
+
   return (
     <div className='App'>
       <img className='background-img' src={Hand} alt='hand' />
@@ -12,25 +25,17 @@ function App() {
         </p>
         <p className='container__quantity-caption'>
           Попалось:
-          <span className='container__quantity-number'> xxx</span>
+          <span className='container__quantity-number'> {visitors}</span>
         </p>
       </div>
       <div className='contacts'>
         <a
-          className='contacts__telega'
-          href='https://t.me/nuncame'
+          className='contacts__donut'
+          href='https://pay.mysbertips.ru/32518683'
           target='_blank'
           rel='noreferrer'
         >
-          @vii
-        </a>
-        <a
-          className='contacts__telega'
-          href='https://t.me/DmitryAlabam'
-          target='_blank'
-          rel='noreferrer'
-        >
-          @alabam
+          <span className='contacts__text'>Донат</span>
         </a>
       </div>
     </div>
